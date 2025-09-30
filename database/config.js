a449cd28422a928ead9e48da984718fff4b8d0e5
MartinCukimai <konamii860@gmail.com>
2025-09-30 20:13:01 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "7683628266:AAFExlwezLhjDOGweb_qeh7qqqH0hLAwW3w",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7565734815", // Ubah Jadi Id Mu !!!
+  port: "3005", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "152.42.162.115" // Ubah Jadi Ip Vps Mu !!!
+};
